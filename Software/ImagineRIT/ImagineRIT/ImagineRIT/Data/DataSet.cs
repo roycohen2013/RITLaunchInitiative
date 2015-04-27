d66011c5fc2c7dada60d4f55ecc8a7cc3f6cbeca
sklei2 <stk1151@rit.edu>
2015-04-27 15:31:23 -0400

    New Windows Form and moved DataSet
    Created a bare bones Windows form that only has a couple labels and
    changed the form's title.
    
    Moved the DataSet.cs class to a Data folder in the project.

---
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Data/DataSet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class DataSet
+{
+    private byte[] Temperatures { get; private set; } //8 bytes
+    private byte NozzleTemp { get; private set; }
+    private byte EngineForce { get; private set; }
+    private byte BarometricPressure { get; private set; }
+    private int NozzlePressure { get; private set; }
+    private byte BatteryVoltage { get; private set; }
+    private bool[] ValvePosition { get; private set; }  //5 booleans
+
+
+    public DataSet(byte[] packetData)
+	{
+        
+	}
+}
---
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ImagineRIT
+{
+    partial class Display
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature (C):";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(603, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temperature";
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Location = new System.Drawing.Point(32, 120);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(0, 17);
+            this.TemperatureLabel.TabIndex = 2;
+            // 
+            // Display
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1297, 558);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Display";
+            this.Text = "Launch Intiative Data Display";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label TemperatureLabel;
+    }
+}
