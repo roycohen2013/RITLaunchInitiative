246e809df77fdda4a07afb342c62b31db9845078
rjd3756 <rjd3756@rit.edu>
2015-04-27 16:49:54 -0400

    Started to try to figure out udp and socket connections

--- Software/ImagineRIT/ImagineRIT/ImagineRIT/Data/DataSet.cs
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Data/DataSet.cs
@@ -2,13 +2,13 @@
 
 public class DataSet
 {
-    private byte[] Temperatures { get; private set; } //8 bytes
-    private byte NozzleTemp { get; private set; }
-    private byte EngineForce { get; private set; }
-    private byte BarometricPressure { get; private set; }
-    private int NozzlePressure { get; private set; }
-    private byte BatteryVoltage { get; private set; }
-    private bool[] ValvePosition { get; private set; }  //5 booleans
+    public byte[] Temperatures { get; private set; } //8 bytes
+    public byte NozzleTemp { get; private set; }
+    public byte EngineForce { get; private set; }
+    public byte BarometricPressure { get; private set; }
+    public int NozzlePressure { get; private set; }
+    public byte BatteryVoltage { get; private set; }
+    public bool[] ValvePosition { get; private set; }  //5 booleans
 
 
     public DataSet(byte[] packetData)
---
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Data/PacketReceiver.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO.Ports;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class PacketReceiver
+{
+    public static void Main()
+    {
+        // This constructor arbitrarily assigns the local port number.
+        UdpClient udpClient = new UdpClient(11000);
+        try
+        {
+            Console.WriteLine("Enter Server");
+            String server = Console.ReadLine();
+            IPHostEntry host = Dns.GetHostEntry(server);
+            IPAddress address = (IPAddress) host.AddressList.GetValue(0);
+            Console.WriteLine("Enter port");
+            int port = Convert.ToInt32(Console.ReadLine());
+            IPEndPoint ipep = new IPEndPoint(address, port);
+            udpClient.Connect(ipep);
+
+            // Sends a message to the host to which you have connected.
+            Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+
+            udpClient.Send(sendBytes, sendBytes.Length);
+
+            // Sends a message to a different host using optional hostname and port parameters.
+            UdpClient udpClientB = new UdpClient();
+            udpClientB.Send(sendBytes, sendBytes.Length, "AlternateHostMachineName", 11000);
+
+            //IPEndPoint object will allow us to read datagrams sent from any source.
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            // Blocks until a message returns on this socket from a remote host.
+            Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+            string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+            // Uses the IPEndPoint object to determine which of these two hosts responded.
+            Console.WriteLine("This is the message you received " +
+                                         returnData.ToString());
+            Console.WriteLine("This message was sent from " +
+                                        RemoteIpEndPoint.Address.ToString() +
+                                        " on their port number " +
+                                        RemoteIpEndPoint.Port.ToString());
+
+            udpClient.Close();
+            udpClientB.Close();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        Console.ReadKey();
+    }
+}
