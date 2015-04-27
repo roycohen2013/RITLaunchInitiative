7116c4bae1e9e781712278a96605b1d7d4ff1239
sklei2 <stk1151@rit.edu>
2015-04-27 17:31:19 -0400

    UI Fix
    - Made it so the runner is the main used. To change this main just go to
    Project->Properties->Application and select Startup Object.
    - Re-Added the UI

---
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Graphulator/Display.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImagineRIT
+{
+    public partial class Display : Form
+    {
+        public Display()
+        {
+            InitializeComponent();
+            this.ShowDialog();
+        }
+
+        private void Display_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void YAxisDropDownToolTip_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+    }
+}
