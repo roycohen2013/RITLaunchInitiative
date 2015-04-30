f8fc8bd1312342f7dfa8b3f1c22407fc91b4a2fa
sklei2 <stk1151@rit.edu>
2015-04-29 22:46:04 -0400

    UIData now has all information and Display updates labels
    Hopfully...

--- Software/ImagineRIT/ImagineRIT/ImagineRIT/Graphulator/Display.cs
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Graphulator/Display.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace ImagineRIT
 {
@@ -17,6 +18,8 @@ namespace ImagineRIT
         public Display()
         {
             InitializeComponent();
+            this.LineGraph.Series.Add("lineGraph");
+            this.LineGraph.Series["lineGraph"].ChartType = SeriesChartType.Line;
             this.ShowDialog();
             UserInterfaceController ui = new UserInterfaceController();
             ui.Subscribe(this); // This is now observing UI controller
@@ -37,21 +40,50 @@ namespace ImagineRIT
             ComboBox box = (ComboBox)sender;
             ui.setGraphIndex(box.SelectedIndex);
             this.LineGraph.Series.Clear(); // empty the graph.
+            this.LineGraph.Series["lineGraph"].AxisLabel = 
+        }
+
+        private void addToGraph(int data)
+        {
+            this.LineGraph.Series["lineGraph"].
         }
 
         public void OnCompleted()
         {
-            throw new NotImplementedException();
+            // we will always get information.
         }
 
         public void OnError(Exception error)
         {
-            throw new NotImplementedException();
+            // NO ERRORS!
         }
 
+        /*
+         * updates all the labels and also passes the data that needs
+         * to be added to the graph.
+         */
         public void OnNext(UIData value)
         {
-            throw new NotImplementedException();
+            this.Pos1LowLevelTemperatureDisplay.Text = value.getPos1Temp().ToString();
+            this.Pos2LowLevelTemperatureDisplay.Text = value.getPos2Temp().ToString();
+            this.Pos3LowLevelTemperatureDisplay.Text = value.getPos3Temp().ToString();
+            this.Pos4LowLevelTemperatureDisplay.Text = value.getPos4Temp().ToString();
+            this.Pos5LowLevelTemperatureDisplay.Text = value.getPos5Temp().ToString();
+            this.Pos6LowLevelTemperatureDisplay.Text = value.getPos6Temp().ToString();
+            this.Pos7LowLevelTemperatureDisplay.Text = value.getPos7Temp().ToString();
+            this.Pos8LowLevelTemperatureDisplay.Text = value.getPos8Temp().ToString();
+            this.NozzlePressureDisplay.Text = value.getNozzlePressure().ToString();
+            this.NozzleTemperatureDisplay.Text = value.getNozzleTemp().ToString();
+            this.EngineForceDisplay.Text = value.getEngineForce().ToString();
+            this.BarometricPressureDisplay.Text = value.getBarometricPressure().ToString();
+
+            this.Pos1ValveDisplay.Text = value.getPos1Valve().ToString();
+            this.Pos2ValveDisplay.Text = value.getPos2Valve().ToString();
+            this.Pos3ValveDisplay.Text = value.getPos3Valve().ToString();
+            this.Pos4ValveDisplay.Text = value.getPos4Valve().ToString();
+            this.Pos5ValveDisplay.Text = value.getPos5Valve().ToString();
+
+            addToGraph(value.getGraphData());
         }
     }
 }
--- Software/ImagineRIT/ImagineRIT/ImagineRIT/Graphulator/UIData.cs
+++ Software/ImagineRIT/ImagineRIT/ImagineRIT/Graphulator/UIData.cs
@@ -9,37 +9,119 @@ namespace ImagineRIT.Graphulator
     // This class is just the data the UI uses to display information.
     public class UIData
     {
-        // Low level temperatures for each position
-        private int pos1temp;
-        private int pos2temp;
-        private int pos3temp;
-        private int pos4temp;
-        private int pos5temp;
-        private int pos6temp;
-        private int pos7temp;
-        private int pos8temp;
+        private int graphIndex; // the index with the data being added
+        private int[] graphable;
+        private bool[] valves;
 
-        private int nozzleTemp;
-        private int engineForce;
-        private int barometricPressure;
-        private int nozzlePressure;
         
-        // valve at each position.
-        private bool pos1valve;
-        private bool pos2valve;
-        private bool pos3valve;
-        private bool pos4valve;
-        private bool pos5valve;
-
-        // The value of what is being added to the graph.
-        private int graph;
-        // the index of the data to be graphed.
-        private int graphIndex;
-        
-        public UIData(int[]graphableData, int index )
+        /*
+         * valvePositions - is a list of true and false values for each valve
+         * position.
+         * graphabledata - is the averages out values of the 100 packets that
+         * could be graphed.
+         * index - which one of the graphable data is going to be graphed.
+         */
+        public UIData(bool[] valvePositions,int[]graphableData, int index )
         {
-            throw new NotImplementedException();
+            valves = valvePositions;
+            graphable = graphableData;
+            graphIndex = index;
         }
 
+        public int getGraphData()
+        {
+            return graphable[graphIndex];
+        }
+
+        public bool getPos1Valve()
+        {
+            return valves[0];
+        }
+
+        public bool getPos2Valve()
+        {
+            return valves[1];
+        }
+
+        public bool getPos3Valve()
+        {
+            return valves[2];
+        }
+
+        public bool getPos4Valve()
+        {
+            return valves[3];
+        }
+
+        public bool getPos5Valve()
+        {
+            return valves[5];
+        }
+
+        public int getPos1Temp()
+        {
+            return graphable[0];
+        }
+
+        public int getPos2Temp()
+        {
+            return graphable[1];
+        }
+
+        public int getPos3Temp()
+        {
+            return graphable[2];
+        }
+
+        public int getPos4Temp()
+        {
+            return graphable[3];
+        }
+
+        public int getPos5Temp()
+        {
+            return graphable[4];
+
+        }
+
+        public int getPos6Temp()
+        {
+            return graphable[5];
+        }
+
+        public int getPos7Temp()
+        {
+            return graphable[6];
+        }
+
+        public int getPos8Temp()
+        {
+            return graphable[7];
+        }
+
+        public int getNozzleTemp()
+        {
+            return graphable[8];
+        }
+
+        public int getEngineForce()
+        {
+            return graphable[9];
+        }
+
+        public int getBarometricPressure()
+        {
+            return graphable[10];
+        }
+
+        public int getNozzlePressure()
+        {
+            return graphable[11];
+        }
+
+        public int getBatteryVoltage()
+        {
+            return graphable[12];
+        }
     }
 }
