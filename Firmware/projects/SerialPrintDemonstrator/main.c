9fbd94ee7e182dc3348590c83c565c48268001d3
roycohen2013 <roycohen122@gmail.com>
2015-08-04 13:22:51 -0400

    adding an example for UARTprintF that works and spams "its billy"

---
+++ Firmware/projects/SerialPrintDemonstrator/main.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"
+
+
+
+
+
+uint32_t ui32SysClkFreq;
+
+
+int main(void)
+{
+	ui32SysClkFreq = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+			SYSCTL_OSC_MAIN | SYSCTL_USE_PLL |
+			SYSCTL_CFG_VCO_480), 120000000);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+	UARTConfigSetExpClk(UART0_BASE, ui32SysClkFreq, 115200,(UART_CONFIG_WLEN_8 |
+			UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+
+	//UARTStdioInit(0); //use UART0, 115200
+    UARTStdioConfig(0, 115200, ui32SysClkFreq);
+
+
+
+	UARTCharPut(UART0_BASE, 'E');
+	UARTCharPut(UART0_BASE, 'n');
+	UARTCharPut(UART0_BASE, 't');
+	UARTCharPut(UART0_BASE, 'e');
+	UARTCharPut(UART0_BASE, 'r');
+	UARTCharPut(UART0_BASE, ' ');
+	UARTCharPut(UART0_BASE, 'T');
+	UARTCharPut(UART0_BASE, 'e');
+	UARTCharPut(UART0_BASE, 'x');
+	UARTCharPut(UART0_BASE, 't');
+	UARTCharPut(UART0_BASE, ':');
+	UARTCharPut(UART0_BASE, ' ');
+
+	while (1)
+	{
+		if (UARTCharsAvail(UART0_BASE)) UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE));
+		UARTprintf("ITS BILLYY text: ");
+
+		//UARTCharPut(UART0_BASE, 't');
+
+	}
+}
