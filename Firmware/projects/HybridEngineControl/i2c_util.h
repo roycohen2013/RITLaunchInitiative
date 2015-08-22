34b0beb9157c93c968c8504fa698c39dbad41e1d
roycohen2013 <roycohen122@gmail.com>
2015-08-22 03:09:05 -0400

    Adding preliminary I2c driver

---
+++ Firmware/projects/HybridEngineControl/i2c_util.h
@@ -0,0 +1,17 @@
+/*
+ * i2c_util.h
+ *
+ *  Created on: Aug 22, 2015
+ *      Author: Roy
+ */
+
+#ifndef I2C_UTIL_H_
+#define I2C_UTIL_H_
+
+
+void InitI2C0(void);
+
+
+
+
+#endif /* I2C_UTIL_H_ */
